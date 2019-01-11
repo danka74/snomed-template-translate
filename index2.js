0ffff8e84fd7aeca2002f244fb8112d4a8d89501
danka74 <daniel.karlsson@socialstyrelsen.se>
2019-01-11 12:25:40 +0100

    all products/drugs translated

--- index2.js
+++ index2.js
@@ -1,10 +1,12 @@
 const rows = require('/home/dlkn02/output.json');
 
-//console.log(rows[0]);
+// tranform SQL result rows to structured concept objects
+// there are one row per relationship
 const concepts = rows.reduce((concepts, row) => {
-    //console.log(concepts);
+    // find concept by id if present
     const concept = concepts.find(c => c.sctid === row.id);
     
+    // if not present push a new object
     if(!concept) {
         concepts.push({
             sctid: row.id,
@@ -18,18 +20,20 @@ const concepts = rows.reduce((concepts, row) => {
                 }]
             } ]
         });
-    } else {
-        if(row.relationshipGroup == 0) {
-            concept.groups.push({
+    } else { // existing concept, update groupes and/or relationships
+        if(row.relationshipGroup == 0) { // first, check if the relationship is in relationship group 0 (RG0)
+            concept.groups.push({ // push a new group object for each RG0 relationship (self-grouping)
                 group: 0,
                 rels: [ {
                     typeId: row.typeId,
                     term: row.term
                 }]
             });
-        } else {
+        } else { // not RG0
+            /// find group if present
             const group = concept.groups.find(g => g.group === row.relationshipGroup);
 
+            // if not present push a new group
             if(!group) {
                 concept.groups.push({
                     group: row.relationshipGroup,
@@ -38,7 +42,7 @@ const concepts = rows.reduce((concepts, row) => {
                         term: row.term
                     }]
                 });
-            } else {
+            } else { // existing group, push relationship
                 group.rels.push({
                     typeId: row.typeId,
                     term: row.term
@@ -54,20 +58,175 @@ const concepts = rows.reduce((concepts, row) => {
 
 console.log("Number of concepts found = " + concepts.length);
 
+var combineTerms = function(total, term, index, length) {
+    if(index == 0) {
+        return total + term;
+    }
+    if(index == length - 1) {
+        return total + " och " + term;
+    }
+    return total + ", " + term;
+}
+
+var numerize = function(word) {
+    if(word === "ett") {
+    return "1";
+    } 
+    if(word === "två") {
+        return "2";
+    } else
+    if(word === "tre") {
+        return "3";
+    } else
+    if(word === "fyra") {
+        return "4";
+    } else
+    if(word === "fem") {
+        return "5";
+    }
+    return word;
+}
+
+// map concept objects to new term objects
 const newTerms = concepts.map(concept => {
-    let term = "läkemedel som innehåller ";
+    let term = "läkemedel som ";
+
+    // count number of "has active ingredient" or "has precise active ingredient" relationships
+    const numberIngredients = concept.groups.reduce((total, group) => {
+        if(group.rels.find(r => r.typeId == 127489000 || r.typeId == 762949000)) {
+            return total + 1;
+        } else {
+            return total;
+        }
+    }, 0);
+
+    // extract the stated number of active ingredients
+    const statedCount = concept.groups.reduce((total, group) => {
+        let rel = group.rels.find(r => r.typeId == 766952006);
+        if(rel) {
+            let term = numerize(rel.term);
+
+            return total + parseInt(term);
+        }
+        return total;   
+    }, 0);
+
+    // if stated ingredient count is not 0 and the numbers match, it is a "containing only" drug
+    if(statedCount != 0 && statedCount == numberIngredients) {
+        term += "endast innehåller ";
+    } else {
+        term += "innehåller ";
+    }
+
+    // mismatch can happen when there are missing translations
+    if(statedCount != 0 && statedCount != numberIngredients) {
+        term += "*saknas översättning*"
+    }
 
     if(concept.semtag === "medicinal product") {
-        term += concept.groups.reduce((total, group) => {
+        const wordList = concept.groups.reduce((total, group) => {
             let rel = group.rels.find(r => r.typeId == 127489000)
-            return total + ", " + (rel ? rel.term : "error!")
-        }, "")
+            if(rel) {
+                total.push(rel.term);
+            }
+            return total;
+        }, [])
+        const words = wordList.reduce((total, word, index, wordList) => {
+            return combineTerms(total, word, index, wordList.length);
+        }, "");
+        term += words;
+    }
+
+    if(concept.semtag === "medicinal product form") {
+        const wordList = concept.groups.reduce((total, group) => {
+            let rel = group.rels.find(r => r.typeId == 127489000)
+            if(rel) {
+                total.push(rel.term);
+            }
+            return total;
+        }, [])
+        const words = wordList.reduce((total, word, index, wordList) => {
+            return combineTerms(total, word, index, wordList.length);
+        }, "");
+        term += words;
+
+        const form = concept.groups.reduce((total, group) => {
+            let rel = group.rels.find(r => r.typeId == 411116001)
+            if(rel) {
+                return rel.term;
+            }
+            return total;
+        }, null);
+        if(form) {
+            term += " i " + form;
+        }
+        
+    }
+
+
+    if(concept.semtag === "clinical drug") {
+        const wordList = concept.groups.reduce((total, group) => {
+            const rel = group.rels.find(r => r.typeId == 127489000)
+            const relPrecisely = group.rels.find(r => r.typeId == 762949000)
+            if(rel || relPrecisely) {
+                let phrase = "";
+
+                if(rel) {
+                    phrase = rel.term;
+                } else
+                if(relPrecisely) {
+                    phrase = "exakt " + relPrecisely.term;
+                }
+
+                const numeratorVal = group.rels.find(r => r.typeId == 732944001 || r.typeId == 733724008);
+                const numeratorUnit = group.rels.find(r => r.typeId == 732945000 || r.typeId == 733725009);
+                const denominatorVal = group.rels.find(r => r.typeId == 732946004 || r.typeId == 733723002);
+                const denominatorUnit = group.rels.find(r => r.typeId == 732947008 || r.typeId == 733722007);
+
+                if(numeratorVal) {
+                    phrase += " " + numerize(numeratorVal.term);
+                }
+                if(numeratorUnit) {
+                    phrase += " " +  numeratorUnit.term;
+                }
+                if(denominatorVal) {
+                    phrase += "/" + numerize(denominatorVal.term);
+                }
+                if(denominatorUnit) {
+                    phrase += " " + denominatorUnit.term;
+                }
+
+                total.push(phrase);
+            }
+
+            return total;
+        }, [])
+
+        const words = wordList.reduce((total, word, index, wordList) => {
+            return combineTerms(total, word, index, wordList.length);
+        }, "");
+        term += words;
+
+        const form = concept.groups.reduce((total, group) => {
+            let rel = group.rels.find(r => r.typeId == 411116001)
+            if(rel) {
+                return rel.term;
+            }
+            return total;
+        }, null);
+        if(form) {
+            term += " i " + form;
+        }
+        
     }
 
     return { 
-                sctid: concepts.sctid,
+                sctid: concept.sctid,
+                fsn: concept.fsn,
                 newTerm: term
     };
 });
 
-console.log(newTerms)
+newTerms.forEach(element => {
+    console.log(element.sctid + "\t" + element.fsn + " \t" + element.newTerm);
+});

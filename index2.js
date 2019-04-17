e1d255309fbc1d4a9dea8f64b37bbe426891d1ff
danka74 <daniel.karlsson@socialstyrelsen.se>
2019-04-17 07:43:48 +0200

    case significance

--- index2.js
+++ index2.js
@@ -1,10 +1,12 @@
-const rows = require('/home/dlkn02/output.json');
+const rows = require('./output.json');
+
+var missingCount = 0;
 
 // tranform SQL result rows to structured concept objects
 // there are one row per relationship
-const concepts = rows.reduce((concepts, row) => {
+const concepts = rows.reduce((concepts, row, index, array) => {
     // find concept by id if present
-    const concept = concepts.find(c => c.sctid === row.id);
+    const concept = concepts.find(c => c.sctid == row.id);
     
     // if not present push a new object
     if(!concept) {
@@ -16,7 +18,8 @@ const concepts = rows.reduce((concepts, row) => {
                 group: row.relationshipGroup,
                 rels: [ {
                     typeId: row.typeId,
-                    term: row.term
+                    term: row.term,
+                    caseSignificanceId: row.caseSignificanceId
                 }]
             } ]
         });
@@ -26,7 +29,8 @@ const concepts = rows.reduce((concepts, row) => {
                 group: 0,
                 rels: [ {
                     typeId: row.typeId,
-                    term: row.term
+                    term: row.term,
+                    caseSignificanceId: row.caseSignificanceId
                 }]
             });
         } else { // not RG0
@@ -39,13 +43,15 @@ const concepts = rows.reduce((concepts, row) => {
                     group: row.relationshipGroup,
                     rels: [ {
                         typeId: row.typeId,
-                        term: row.term
+                        term: row.term,
+                        caseSignificanceId: row.caseSignificanceId
                     }]
                 });
             } else { // existing group, push relationship
                 group.rels.push({
                     typeId: row.typeId,
-                    term: row.term
+                    term: row.term,
+                    caseSignificanceId: row.caseSignificanceId
                 });
             }
             
@@ -58,7 +64,7 @@ const concepts = rows.reduce((concepts, row) => {
 
 console.log("Number of concepts found = " + concepts.length);
 
-var combineTerms = function(total, term, index, length) {
+const combineTerms = function(total, term, index, length) {
     if(index == 0) {
         return total + term;
     }
@@ -68,7 +74,7 @@ var combineTerms = function(total, term, index, length) {
     return total + ", " + term;
 }
 
-var numerize = function(word) {
+const numerize = function(word) {
     if(word === "ett") {
     return "1";
     } 
@@ -84,12 +90,39 @@ var numerize = function(word) {
     if(word === "fem") {
         return "5";
     }
+    if(word === "sex") {
+        return "6";
+    } 
+    if(word === "sju") {
+        return "7";
+    } else
+    if(word === "åtta") {
+        return "8";
+    } else
+    if(word === "nio") {
+        return "9";
+    } else
+    if(word === "tio") {
+        return "10";
+    }
     return word;
+}
+
+// initially every term is considered case insensitive, the intial character is always case insensitive, iff there is a case sensitive term as part, then the intire term is "Only initial..."
+const aggregateCS = function(currentCS, newCS) {
+    if(currentCS == 900000000000020002) { // | Only initial character case insensitive (core metadata concept) |
+        return currentCS;
+    }
+    if(newCS == 900000000000020002 || newCS == 900000000000017005) { // | Entire term case sensitive (core metadata concept) |
+        return 900000000000020002; // | Only initial character case insensitive (core metadata concept) |
+    }
+    return currentCS;
 }
 
 // map concept objects to new term objects
 const newTerms = concepts.map(concept => {
     let term = "läkemedel som ";
+    let caseSignificanceId = 900000000000448009;
 
     // count number of "has active ingredient" or "has precise active ingredient" relationships
     const numberIngredients = concept.groups.reduce((total, group) => {
@@ -102,7 +135,7 @@ const newTerms = concepts.map(concept => {
 
     // extract the stated number of active ingredients
     const statedCount = concept.groups.reduce((total, group) => {
-        let rel = group.rels.find(r => r.typeId == 766952006);
+        let rel = group.rels.find(r => r.typeId == 766952006); // TODO: base active ingredient...
         if(rel) {
             let term = numerize(rel.term);
 
@@ -120,14 +153,16 @@ const newTerms = concepts.map(concept => {
 
     // mismatch can happen when there are missing translations
     if(statedCount != 0 && statedCount != numberIngredients) {
-        term += "*saknas översättning*"
+        term += "*saknas översättning*";
+        missingCount++;
     }
 
-    if(concept.semtag === "medicinal product") {
+    if(concept.semtag === "medicinal product" || concept.semtag === "product") {
         const wordList = concept.groups.reduce((total, group) => {
-            let rel = group.rels.find(r => r.typeId == 127489000)
+            let rel = group.rels.find(r => r.typeId == 127489000 || r.typeId == 762949000) // 127489000 | Has active ingredient (attribute) |, 762949000 | Has precise active ingredient (attribute) |
             if(rel) {
                 total.push(rel.term);
+                caseSignificanceId = aggregateCS(caseSignificanceId, rel.caseSignificanceId);
             }
             return total;
         }, [])
@@ -139,9 +174,10 @@ const newTerms = concepts.map(concept => {
 
     if(concept.semtag === "medicinal product form") {
         const wordList = concept.groups.reduce((total, group) => {
-            let rel = group.rels.find(r => r.typeId == 127489000)
+            let rel = group.rels.find(r => r.typeId == 127489000 || r.typeId == 762949000) // 127489000 | Has active ingredient (attribute) |, 762949000 | Has precise active ingredient (attribute) |
             if(rel) {
                 total.push(rel.term);
+                caseSignificanceId = aggregateCS(caseSignificanceId, rel.caseSignificanceId);
             }
             return total;
         }, [])
@@ -151,8 +187,9 @@ const newTerms = concepts.map(concept => {
         term += words;
 
         const form = concept.groups.reduce((total, group) => {
-            let rel = group.rels.find(r => r.typeId == 411116001)
+            let rel = group.rels.find(r => r.typeId == 411116001) // 411116001 | Has manufactured dose form (attribute) |
             if(rel) {
+                caseSignificanceId = aggregateCS(caseSignificanceId, rel.caseSignificanceId);
                 return rel.term;
             }
             return total;
@@ -166,16 +203,18 @@ const newTerms = concepts.map(concept => {
 
     if(concept.semtag === "clinical drug") {
         const wordList = concept.groups.reduce((total, group) => {
-            const rel = group.rels.find(r => r.typeId == 127489000)
-            const relPrecisely = group.rels.find(r => r.typeId == 762949000)
+            const rel = group.rels.find(r => r.typeId == 127489000) // 127489000 | Has active ingredient (attribute) |
+            const relPrecisely = group.rels.find(r => r.typeId == 762949000) // 762949000 | Has precise active ingredient (attribute) |
             if(rel || relPrecisely) {
                 let phrase = "";
 
                 if(rel) {
                     phrase = rel.term;
+                    caseSignificanceId = aggregateCS(caseSignificanceId, rel.caseSignificanceId);
                 } else
                 if(relPrecisely) {
                     phrase = "exakt " + relPrecisely.term;
+                    caseSignificanceId = aggregateCS(caseSignificanceId, relPrecisely.caseSignificanceId);
                 }
 
                 const numeratorVal = group.rels.find(r => r.typeId == 732944001 || r.typeId == 733724008);
@@ -188,12 +227,18 @@ const newTerms = concepts.map(concept => {
                 }
                 if(numeratorUnit) {
                     phrase += " " +  numeratorUnit.term;
+                    caseSignificanceId = aggregateCS(caseSignificanceId, numeratorUnit.caseSignificanceId);
                 }
-                if(denominatorVal) {
-                    phrase += "/" + numerize(denominatorVal.term);
-                }
-                if(denominatorUnit) {
-                    phrase += " " + denominatorUnit.term;
+                if(denominatorVal && denominatorUnit && denominatorVal.term === "ett") {
+                    phrase += "/" + denominatorUnit.term;
+                } else {
+                    if(denominatorVal) {
+                        phrase += "/" + numerize(denominatorVal.term);
+                    }
+                    if(denominatorUnit) {
+                        phrase += " " + denominatorUnit.term;
+                        caseSignificanceId = aggregateCS(caseSignificanceId, denominatorUnit.caseSignificanceId);
+                    }
                 }
 
                 total.push(phrase);
@@ -210,6 +255,7 @@ const newTerms = concepts.map(concept => {
         const form = concept.groups.reduce((total, group) => {
             let rel = group.rels.find(r => r.typeId == 411116001)
             if(rel) {
+                caseSignificanceId = aggregateCS(caseSignificanceId, rel.caseSignificanceId);
                 return rel.term;
             }
             return total;
@@ -223,10 +269,13 @@ const newTerms = concepts.map(concept => {
     return { 
                 sctid: concept.sctid,
                 fsn: concept.fsn,
-                newTerm: term
+                newTerm: term,
+                caseSignificanceId: caseSignificanceId
     };
 });
 
+console.log("Concept ID	GB/US FSN Term	Translated Term	Language Code	Case significance	Type	Language reference set	Acceptability");
+
 newTerms.forEach(element => {
-    console.log(element.sctid + "\t" + element.fsn + " \t" + element.newTerm);
-});
+    console.log(element.sctid + "\t" + element.fsn + " \t" + element.newTerm + "\tsv\t" + (element.caseSignificanceId === 900000000000448009 ? "ci" : "cI") + "\tSYNONYM\tSwedish\tPREFERRED");
+}); 
